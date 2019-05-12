302aeecd86031bc11fd6d2a20355450fcce17424
PB <46657932+EVSalomon@users.noreply.github.com>
2019-05-12 21:24:54 +0200

    iPhone X+ Support

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -33,4 +33,70 @@
 .v-bottom-nav {
     background-color: white !important;
 }
+
+/* iPhone XR Portrait*/
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 2)
+and (orientation: portrait) {
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
+
+/* iPhone X,XS Portrait*/
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 3)
+and (orientation: portrait) {
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
+
+/* iPhone XS Max Portrait*/
+@media only screen
+and (device-width : 414px)
+and (device-height : 896px)
+and (-webkit-device-pixel-ratio : 3)
+and (orientation: portrait){
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
+
+/* iPhone XR Landscape*/
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 2)
+and (orientation: landscape) {
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
+
+/* iPhone X,XS Landscape*/
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 3)
+and (orientation: landscape) {
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
+
+/* iPhone XS Max Landscape*/
+@media only screen
+and (device-width : 414px)
+and (device-height : 896px)
+and (-webkit-device-pixel-ratio : 3)
+and (orientation: landscape){
+    .v-item-group.v-bottom-nav--fixed {
+        height:71px !important;
+    }
+}
 </style>
