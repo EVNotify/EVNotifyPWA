66c625a932df415c31ebcd4bda87ea1eea84a582
Pazekal90 <mail@pascal-pischel.de>
2019-05-31 15:59:27 +0200

    Set FinishTime into brackets instead of a slash

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -39,7 +39,7 @@
                     <v-icon color="teal">schedule</v-icon>
                   </v-list-tile-action>
                   <v-list-tile-content>
-                    <v-list-tile-title>{{ chargingTimeLeft }} h / {{ finishTime }}</v-list-tile-title>
+                    <v-list-tile-title>{{ chargingTimeLeft }} h ({{ finishTime }})</v-list-tile-title>
                   </v-list-tile-content>
                 </v-list-tile>
               </v-list>
