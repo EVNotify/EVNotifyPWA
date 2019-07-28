7d3ae6d6536180067025adbf2de2b8774b9de158
Pazekal90 <mail@pascal-pischel.de>
2019-07-28 13:48:35 +0200

    Changed bottom navigation to not be shown side by side with upper layout container.

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -1,24 +1,22 @@
 <template>
-    <v-layout>
-        <v-bottom-nav :value="true" fixed color="transparent">
-            <v-btn flat color="teal" value="Dashboard" to="/">
-                <span>Dashboard</span>
-                <v-icon>dashboard</v-icon>
-            </v-btn>
-            <v-btn flat color="teal" value="Logs" to="/logs">
-                <span>Logs</span>
-                <v-icon>list</v-icon>
-            </v-btn>
-            <v-btn flat color="teal" value="Stations">
-                <span>Stations</span>
-                <v-icon>ev_station</v-icon>
-            </v-btn>
-            <v-btn flat color="teal" value="Settings" to="/settings">
-                <span>Settings</span>
-                <v-icon>settings</v-icon>
-            </v-btn>
-        </v-bottom-nav>
-    </v-layout>
+    <v-bottom-nav :value="true" fixed color="transparent">
+        <v-btn flat color="teal" value="Dashboard" to="/">
+            <span>Dashboard</span>
+            <v-icon>dashboard</v-icon>
+        </v-btn>
+        <v-btn flat color="teal" value="Logs" to="/logs">
+            <span>Logs</span>
+            <v-icon>list</v-icon>
+        </v-btn>
+        <v-btn flat color="teal" value="Stations">
+            <span>Stations</span>
+            <v-icon>ev_station</v-icon>
+        </v-btn>
+        <v-btn flat color="teal" value="Settings" to="/settings">
+            <span>Settings</span>
+            <v-icon>settings</v-icon>
+        </v-btn>
+    </v-bottom-nav>
 </template>
 
 <script>
